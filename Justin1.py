fd8a5e6277a3a57469d646c0fcd122ff67838f22
Vaibhav Raj <whybebhav@gmail.com>
2026-02-07 00:25:11 +0530

    three links

--- Justin1.py
+++ Justin1.py
@@ -4,15 +4,17 @@ from bs4 import BeautifulSoup
 import re
 
 def extract_soi_table(url, num):
+    print(num)
     bdc_html = requests.get(url, headers=headers).text
     soup = BeautifulSoup(bdc_html, "lxml")
 
     # soi_p_tag = soup.find("p", id="consolidated_schedule_investments")
-    soi_p_tag = soup.find("p", id=is_soi_id)
+    soi_p_tag = soup.find("p", id=re.compile(r'^(?=.*consolidated)(?=.*schedule)(?=.*investments).*', re.IGNORECASE))
     soi_table = soi_p_tag.find_next("table")
 
     soi_df = pd.read_html(str(soi_table))[0]
     soi_df.to_csv(f"soi_table{num}.csv", index=False)
+    print(f"soi_table{num}.csv saved successfully!")
 
 
 def is_soi_id(id):
@@ -29,11 +31,11 @@ headers = {
 }
 
 urls = [
-    "https://www.sec.gov/ix?doc=/Archives/edgar/data/1675033/000119312525264873/gecc-20250930.htm",
-        "https://www.sec.gov/ix?doc=/Archives/edgar/data/0001925309/000119312525266848/ck0001925309-20250930.htm",
-        "https://www.sec.gov/ix?doc=/Archives/edgar/data/0001370755/000119312525268125/tcpc-20250930.htm",
-        "https://www.sec.gov/ix?doc=/Archives/edgar/data/0001487428/000143774925031988/hrzn20250930_10q.htm"
-]
+        "https://www.sec.gov/Archives/edgar/data/1675033/000119312525264873/gecc-20250930.htm",
+        "https://www.sec.gov/Archives/edgar/data/0001925309/000119312525266848/ck0001925309-20250930.htm",
+        "https://www.sec.gov/Archives/edgar/data/0001370755/000119312525268125/tcpc-20250930.htm",
+        "https://www.sec.gov/Archives/edgar/data/0001487428/000143774925031988/hrzn20250930_10q.htm"
+    ]
 
 for i, url in enumerate(urls):
     extract_soi_table(url, i)
